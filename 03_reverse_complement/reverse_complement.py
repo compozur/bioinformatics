7fa8ce8bd8e7472ec3ed99057647ced57068f105
Compozur <>
2024-01-22 16:59:41 -0800

    Add DNA reverse complement

---
+++ 03_reverse_complement/reverse_complement.py
@@ -0,0 +1,9 @@
+def reverse_complement(dna_sequence):
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    return ''.join([complement[nucleotide] for nucleotide in dna_sequence])
+
+dna_seq = "AGAGATTCGTTACTAC"
+
+rev_comp = reverse_complement(dna_seq)
+print(f"Reverse Complement: {rev_comp}")
+

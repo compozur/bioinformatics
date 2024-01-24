053a8ea9e702c110b2bd90cdaf627758b1b02b2d
Compozur <>
2024-01-23 17:55:56 -0800

    Add reading FASTA files tutorial

---
+++ 04_read_fasta/read_fasta.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+
+def read_fasta(file_path, fmt="fasta"):
+    with open(file_path) as handle:
+        for record in SeqIO.parse(handle, fmt):
+            print(record.id)
+            print(record.seq)
+            print(len(record))
+            print(record.reverse_complement().seq)
+            
+fasta_file = "example.fasta"
+read_fasta(fasta_file)
